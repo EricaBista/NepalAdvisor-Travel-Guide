9ce8446bd82f5c6ff4bce7cfb8e8a098f0311a95
ericaROR <erica@nepaldigital.com>
2015-02-03 09:58:22 +0545

    fixed bug

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,11 +4,9 @@ class Item < ActiveRecord::Base
 	has_many :descriptions
 	has_many :images
 	belongs_to :category
-<<<<<<< HEAD
-	accepts_nested_attributes_for :images, :descriptions
-=======
+
 
 	accepts_nested_attributes_for :images
 	accepts_nested_attributes_for :descriptions
->>>>>>> 8dba7ad32162a1f4e38997dafca44ddd66690967
+
 end
