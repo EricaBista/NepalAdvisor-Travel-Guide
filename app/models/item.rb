f1178a1cecee4cfdda531e4ac50572d9f7060480
kuber <kuber@nepaldigital.com>
2015-02-04 09:48:31 +0545

    search added
    times search added

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,4 +9,10 @@ class Item < ActiveRecord::Base
 	accepts_nested_attributes_for :images
 	accepts_nested_attributes_for :descriptions
 
+	scope :item_search, -> (query) { 
+	 		joins("LEFT JOIN `descriptions` ON descriptions.item_id = items.id")
+	 		.joins("LEFT JOIN `images` ON images.item_id = items.id")
+	 		.where("Name like ? OR Description like ? OR descriptions.Title like ? OR descriptions.Content like ? OR images.Title like ? OR images.Content like ?", 
+	 	"%#{query}%","%#{query}%","%#{query}%","%#{query}%","%#{query}%","%#{query}%").group('items.id') }
+
 end
