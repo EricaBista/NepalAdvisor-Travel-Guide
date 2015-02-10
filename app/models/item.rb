fac9565f0eb10174548262ade646436b3de60d19
ericaROR <erica@nepaldigital.com>
2015-02-10 17:26:24 +0545

    search

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,7 +15,7 @@ class Item < ActiveRecord::Base
 	scope :item_search, -> (query) { 
 	 		joins("LEFT JOIN descriptions ON descriptions.item_id = items.id")
 	 		.joins("LEFT JOIN images ON images.item_id = items.id")
-	 		.where("Name like ? OR Description like ? OR descriptions.Title like ? OR descriptions.Content like ? OR images.Title like ? OR images.Content like ?", 
+	 		.where("items.Name like ? OR Description like ? OR descriptions.Title like ? OR descriptions.Content like ? OR images.Title like ? OR images.Content like ?", 
 	 	"%#{query}%","%#{query}%","%#{query}%","%#{query}%","%#{query}%","%#{query}%").group('items.id') }
 
 end
