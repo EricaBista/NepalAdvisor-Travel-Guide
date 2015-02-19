47e35b32962f66e3adfb178f86e5c4d03b0100d6
ericaROR <erica@nepaldigital.com>
2015-02-19 13:59:04 +0545

    items fixes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150213091952) do
+ActiveRecord::Schema.define(version: 20150216133201) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -102,6 +102,22 @@ ActiveRecord::Schema.define(version: 20150213091952) do
     t.datetime "updated_at"
     t.string   "short_tag"
   end
+
+  create_table "redactor_assets", force: true do |t|
+    t.string   "data_file_name",               null: false
+    t.string   "data_content_type"
+    t.integer  "data_file_size"
+    t.integer  "assetable_id"
+    t.string   "assetable_type",    limit: 30
+    t.string   "type",              limit: 30
+    t.integer  "width"
+    t.integer  "height"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "redactor_assets", ["assetable_type", "assetable_id"], name: "idx_redactor_assetable"
+  add_index "redactor_assets", ["assetable_type", "type", "assetable_id"], name: "idx_redactor_assetable_type"
 
   create_table "restaurants", force: true do |t|
     t.string   "Name"
