f84b30b04744a51699216597ffe33d4ed3817e6f
kuber <kuber@nepaldigital.com>
2015-02-25 16:03:13 +0545

    change password, horizontal ads partial added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,12 +8,14 @@ class UsersController < ApplicationController
 
   def update_password
     @user = User.find(current_user.id)
-    if @user.update_with_password(user_params)
+    # puts "test test test test test test"
+    if @user.update(user_params)
       # Sign in the user by passing validation in case their password changed
       sign_in @user, :bypass => true
       redirect_to root_path
     else
-      render "edit"
+
+      render "update_password"
     end
   end
    
@@ -56,4 +58,6 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:email, :password, :password_confirmation, :current_password, :picture, :full_name)
     end
+
+
 end
