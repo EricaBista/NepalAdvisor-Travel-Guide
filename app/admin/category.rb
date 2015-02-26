3f2058837a4f11b7cdb8281bce4929d5063a6933
kuber <kuber@nepaldigital.com>
2015-02-26 09:36:47 +0545

    heroku test

--- app/admin/category.rb
+++ app/admin/category.rb
@@ -5,32 +5,7 @@ config.sort_order = 'position_asc'
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
    permit_params :list, :of, :attributes, :on, :model, :Name, :Description, :icon,  :Order, :is_menu, :is_destination
- index do 
-     
-     column :Name do |category|
-      content_tag(:span, category.Name, :"data-id" => category.id, :class => "category")
-      end
-     column :Order
-     column :slug
-     # column :icon  
-     column "icon" do |category|
-      image_tag category.icon_url(:avatar)
-     end
-     # column :Description
-     # column "" do |resource|
-     #  links = ''.html_safe
-     #  links += link_to I18n.t('active_admin.view'), resource_path(resource), :class => "member_link view_link"
-     #  links += link_to I18n.t('active_admin.edit'), edit_resource_path(resource), :class => "member_link edit_link"
-     #  links += link_to I18n.t('active_admin.delete'), resource_path(resource), :method => :delete, :confirm => I18n.t('active_admin.delete_confirmation'), :class => "member_link delete_link"
-     #  links
-     #end
-     actions
-    
-    # column :category do |c|
-     
-    #     c.category.Name
 
-       end
   # or
    form do |f|
       f.inputs "Category" do
