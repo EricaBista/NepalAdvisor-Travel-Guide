d1c7bb8beb3e3f908ec0c14f1bc5757d104e6e38
kuber <kuber@nepaldigital.com>
2015-03-03 10:10:05 +0545

    ignore upload

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,54 +21,54 @@
 
 
 $(function() {
-  $('a#add-another').click(function() {
-    $('#description-list li:first').clone().find('input').val('')
-    .end().appendTo('#description-list');
-  });
+  // $('a#add-another').click(function() {
+  //   $('#description-list li:first').clone().find('input').val('')
+  //   .end().appendTo('#description-list');
+  // });
 
-  $(document).on("click", '.delete-description', function() {
-    if ($('#description-list li').length > 1)
-  		$(this).parent().remove();
-    else
-  		alert('You need at least one description.')
-  });
+  // $(document).on("click", '.delete-description', function() {
+  //   if ($('#description-list li').length > 1)
+  // 		$(this).parent().remove();
+  //   else
+  // 		alert('You need at least one description.')
+  // });
 
    
  });
 
-function readURL(input) {
-alert(1);
-        if (input.files && input.files[0]) {
-            var reader = new FileReader();
+// function readURL(input) {
+// alert(1);
+//         if (input.files && input.files[0]) {
+//             var reader = new FileReader();
             
-            reader.onload = function (e) {
-                $('#blah').attr('src', e.target.result);
+//             reader.onload = function (e) {
+//                 $('#blah').attr('src', e.target.result);
                  
-            }
+//             }
             
-            reader.readAsDataURL(input.files[0]);
-        }
-    }
+//             reader.readAsDataURL(input.files[0]);
+//         }
+//     }
 
-function serializeArtworks(){
-  var categories = $.makeArray(
-    $("#categories .category").map(function(){
-      return $(this).data("id");
-    })
-  );
-  return {ids: artworkIds};
-};
+// function serializeArtworks(){
+//   var categories = $.makeArray(
+//     $("#categories .category").map(function(){
+//       return $(this).data("id");
+//     })
+//   );
+//   return {ids: artworkIds};
+// };
 
-$("#index_table_categories tbody").sortable({
-  update: function(){
-    $.ajax({
-      url: "/admin/categories/sort",
-      type: 'post',
-      data: serializeArtworks(),
-      complete: function(){
-        $(".paginated_collection").effect("highlight");
-      }
-    });
-  }
-});
+// $("#index_table_categories tbody").sortable({
+//   update: function(){
+//     $.ajax({
+//       url: "/admin/categories/sort",
+//       type: 'post',
+//       data: serializeArtworks(),
+//       complete: function(){
+//         $(".paginated_collection").effect("highlight");
+//       }
+//     });
+//   }
+// });
 //= require_tree .
