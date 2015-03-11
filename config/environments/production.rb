89bcd9c4c0cc5afd52ac801b848092c2f1710975
ericaROR <erica@nepaldigital.com>
2015-03-11 16:59:14 +0545

    fixed error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
-  config.assets.precompile += %w[tinymce/tinymce.min.js tinymce/themes/modern/theme.min.js]
+  config.assets.precompile += %w[tinymce/tinymce.min.js assets/javascripts/tinymce/themes/modern/theme.min.js]
 
   # Generate digests for assets URLs.
   config.assets.digest = true
