b1675ff010f9faecce0b26ed11c343fb2799ab8a
ericaROR <erica@nepaldigital.com>
2015-03-11 09:59:26 +0545

    tinymce

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -5,7 +5,7 @@ ActiveAdmin.setup do |config|
   # for each of the active admin pages.
   #
   config.site_title = "Nepal Adviser"
-  
+  config.register_javascript 'tinymce/tiny_mce.js'
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
   #
