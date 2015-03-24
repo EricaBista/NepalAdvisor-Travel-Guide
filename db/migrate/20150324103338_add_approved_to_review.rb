47460a78b0195f2a42d130386528a08071e59ead
ericaROR <erica@nepaldigital.com>
2015-03-24 18:07:46 +0545

    fixed error

--- db/migrate/20150324103338_add_approved_to_review.rb
+++ db/migrate/20150324103338_add_approved_to_review.rb
@@ -2,5 +2,6 @@ class AddApprovedToReview < ActiveRecord::Migration
   def change
   	unless column_exists? :users, :approved
     add_column :reviews, :approved, :boolean, :default => false
+end
   end
 end
