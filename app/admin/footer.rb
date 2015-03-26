3275b92d0dd2b3274bbef1cbb1a6c53abf856231
ericaROR <erica@nepaldigital.com>
2015-03-26 12:46:39 +0545

    dashboard

--- app/admin/footer.rb
+++ app/admin/footer.rb
@@ -1,7 +1,6 @@
 module ActiveAdmin
   module Views
     class Footer < Component
-
       def build
         super :id => "footer"                                                    
         super :style => "text-align: right;"                                     
