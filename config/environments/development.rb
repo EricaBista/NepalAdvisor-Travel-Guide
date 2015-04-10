5cf991e29468a1e34a6d61b7a73b43df3d5d0f5c
ericaROR <erica@nepaldigital.com>
2015-04-10 15:26:43 +0545

    heroku error

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,9 +42,9 @@ Rails.application.configure do
   # config.action_mailer.delivery_method = :smtp
   # config.action_mailer.smtp_settings = { :address => "127.0.0.1", :port => 1025 }
 
-config.action_mailer.default_url_options = {:host => "localhost:3000"}
+   config.action_mailer.default_url_options = {:host => "localhost:3000"}
    config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
+   config.action_mailer.smtp_settings = {
     :address => "smtp.gmail.com",
     :port => "587",
     :domain => "127.0.0.1",
