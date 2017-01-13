f52325d66b713816f3e7b109db8ca56bc020f0ca
Erica Bista <ericabista77@gmail.com>
2017-01-13 12:15:57 +1100

    Update destinations_controller.rb

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,9 +1,7 @@
 class DestinationsController < InheritedResources::Base
-
   private
 
-    def destination_params
-      params.require(:destination).permit(:Name, :Title, :Content)
-    end
+  def destination_params
+    params.require(:destination).permit(:Name, :Title, :Content)
+  end
 end
-
