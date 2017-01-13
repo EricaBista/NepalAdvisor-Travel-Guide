7e0f5f6418c5258aa9abbbc753ea48bb61280964
Erica Bista <ericabista77@gmail.com>
2017-01-13 12:18:53 +1100

    Update contacts_controller.rb

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,24 +1,23 @@
 class ContactsController < InheritedResources::Base
-	def About_us
-		@contacts = Contact.find(params[:id]) 
-	end
-	def site_map
-	end
+  
+  def About_us
+    @contacts = Contact.find(params[:id]) 
+  end
+  
+  def site_map
+  end
 
-    def cms
-      @contact = Contact.find_by_Slug!(params[:slug])
-       
-    end
+  def cms
+    @contact = Contact.find_by_Slug!(params[:slug])  
+  end
 
-
-	def index
-		@contacts = Contact.all
-	end
+  def index
+    @contacts = Contact.all
+  end
 
   private
 
-    def contact_params
-      params.require(:contact).permit(:Slug, :Title, :Content, :Order, :Home_page, :slug)
-    end
+  def contact_params
+    params.require(:contact).permit(:Slug, :Title, :Content, :Order, :Home_page, :slug)
+  end
 end
-
