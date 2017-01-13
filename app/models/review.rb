c7a086317b1aec6a7796f384ffad7fdb431e16bf
Erica Bista <ericabista77@gmail.com>
2017-01-13 14:58:19 +1100

    Update review.rb

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,15 +1,15 @@
 class Review < ActiveRecord::Base
+  before_save :add_user_session
+
   validates :user_id, presence: true
   validates :title, presence: true
   validates :description, presence: true
+
   has_many :users
   belongs_to :item
-  #before_filter :add_user_session
-  before_save :add_user_session
-
-
+  
   def add_user_session
-  	   #self.user_id= current_user
-  	   #puts current_user.inspect
+    #self.user_id= current_user
+    #puts current_user.inspect
   end
 end
