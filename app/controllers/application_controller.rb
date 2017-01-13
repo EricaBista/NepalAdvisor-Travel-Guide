2bf6cffdc8f28fdca7ca232af60b23da10f97fe4
Erica Bista <ericabista77@gmail.com>
2017-01-13 12:24:32 +1100

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,63 +1,55 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-
   protect_from_forgery with: :exception
   layout :layout
   before_filter :set_menu
   before_filter :configure_permitted_parameters, if: :devise_controller?
   before_filter :store_location
 
-def store_location
-  # store last url - this is needed for post-login redirect to whatever the user last visited.
-  return unless request.get? 
-  if (request.path != "/users/sign_in" &&
+  def store_location
+    return unless request.get? 
+    if (request.path != "/users/sign_in" &&
       request.path != "/users/sign_up" &&
       request.path != "/users/password/new" &&
       request.path != "/users/password/edit" &&
       request.path != "/users/confirmation" &&
       request.path != "/users/sign_out" &&
       !request.xhr?) # don't store ajax calls
-    session[:previous_url] = request.fullpath 
+      session[:previous_url] = request.fullpath 
+    end
   end
-end
 
-def after_sign_in_path_for(resource)
-  session[:previous_url] || root_path
-end
+  def after_sign_in_path_for(resource)
+    session[:previous_url] || root_path
+  end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:account_update) { |u| 
-      u.permit(:password, :password_confirmation, :current_password) 
-    }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:password, :password_confirmation, :current_password) }
   end
   
-   def layout
+  def layout
     if controller_name == "home_controller"
       layout 'home'
     end
-   end
+  end
 
-   def authenticate_active_admin_user!
-        authenticate_user!
-        if current_user.role?(:normal)
-            flash[:alert] = "You are not authorized to access this resource!"
-            redirect_to root_path
-          
-        end
+  def authenticate_active_admin_user!
+    authenticate_user!
+    if current_user.role?(:normal)
+      flash[:alert] = "You are not authorized to access this resource!"
+      redirect_to root_path
     end
+  end
 
   private
   
-   def set_menu
-        @menus ||= Category.where(:is_menu => true).order(:Order)
-        @footer_menus ||= Contact.where(:Home_page => true).order(:Order)
-        @quicklinks ||= Contact.find_by_Slug("quick-links")
-        @more ||= Contact.find_by_Slug("more")
-        @restaurant ||= Category.find_by_slug("restaurants")
-        @thingstodo ||= Category.find_by_slug("thingstodo")
-        @vertical_advertisement ||= Advertisement.where(:position => "vertical").where("(started_date <= ? AND ended_date >= ?) OR (clicked <= max_clicked)", Date.today, Date.today).limit(3).order("RANDOM()")
-        @horizontal_advertisement ||= Advertisement.where(:position => "horizontal").where("(started_date <= ? AND ended_date >= ?) OR (clicked <= max_clicked)", Date.today, Date.today).order("RANDOM()").limit(1)
-        
-   end
+  def set_menu
+    @menus ||= Category.where(:is_menu => true).order(:Order)
+    @footer_menus ||= Contact.where(:Home_page => true).order(:Order)
+    @quicklinks ||= Contact.find_by_Slug("quick-links")
+    @more ||= Contact.find_by_Slug("more")
+    @restaurant ||= Category.find_by_slug("restaurants")
+    @thingstodo ||= Category.find_by_slug("thingstodo")
+    @vertical_advertisement ||= Advertisement.where(:position => "vertical").where("(started_date <= ? AND ended_date >= ?) OR (clicked <= max_clicked)", Date.today, Date.today).limit(3).order("RANDOM()")
+    @horizontal_advertisement ||= Advertisement.where(:position => "horizontal").where("(started_date <= ? AND ended_date >= ?) OR (clicked <= max_clicked)", Date.today, Date.today).order("RANDOM()").limit(1)   
+  end
 end
