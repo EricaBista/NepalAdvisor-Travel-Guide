abf52970e2e3fde99874b9c26ed57139c46a00d0
Erica Bista <ericabista77@gmail.com>
2017-01-13 14:54:14 +1100

    Update contact.rb

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,9 +1,10 @@
 class Contact < ActiveRecord::Base
-	validates :Title, presence: true
-	validates :Slug, uniqueness: true
-	validates :Content, presence: true
-	 def create_slug
-	    self.Slug = self.Title.parameterize
-	  end
-	  before_save :create_slug
+  before_save :create_slug
+  validates :Title, presence: true
+  validates :Slug, uniqueness: true
+  validates :Content, presence: true
+  
+  def create_slug
+    self.Slug = self.Title.parameterize
+  end   
 end
