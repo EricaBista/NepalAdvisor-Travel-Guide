ec8904da78c833e87a38193f07a8ef045996d05e
Erica Bista <ericabista77@gmail.com>
2017-01-13 12:15:10 +1100

    Update home_controller.rb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,8 @@
 class HomeController < ApplicationController
-	def index
-	@item = Item.all 
-	# @restaurants = Restaurant.all
-	@destinations = Category.where(:is_destination => true).order(:Order)
-	@things_to_do = ThingsToDo.all
-	#@categories = Category.all
-	end
-	
+  
+  def index
+    @item = Item.all 
+    @destinations = Category.where(:is_destination => true).order(:Order)
+    @things_to_do = ThingsToDo.all
+  end
 end
