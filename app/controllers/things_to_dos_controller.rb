1ded77b9ba6e896d238ec3c01dd252eb388d0d20
Erica Bista <ericabista77@gmail.com>
2017-01-13 11:59:33 +1100

    Update things_to_dos_controller.rb

--- app/controllers/things_to_dos_controller.rb
+++ app/controllers/things_to_dos_controller.rb
@@ -1,9 +1,7 @@
 class ThingsToDosController < InheritedResources::Base
-
   private
 
-    def things_to_do_params
-      params.require(:things_to_do).permit(:Name, :Title, :Content)
-    end
+  def things_to_do_params
+    params.require(:things_to_do).permit(:Name, :Title, :Content)
+  end
 end
-
