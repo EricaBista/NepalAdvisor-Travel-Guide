e58619303a260ce5eebaa6c01e55c8dd1705cedc
Erica Bista <ericabista77@gmail.com>
2017-01-13 14:56:44 +1100

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,28 +1,26 @@
 class Item < ActiveRecord::Base
-	before_save :create_slug
-	def create_slug
-	    self.slug = self.Name.parameterize
-	  end
-	validates :Name, presence: true
-	validates :Description, presence: true
-	has_many :descriptions
-	has_many :images
-	has_many :reviews
-	belongs_to :category
-	has_many :likes
+  before_save :create_slug
+  belongs_to :category
+  has_many :descriptions
+  has_many :images
+  has_many :reviews
+  has_many :likes
 
-	accepts_nested_attributes_for :images, :allow_destroy => true
-	accepts_nested_attributes_for :descriptions, :allow_destroy => true
-	accepts_nested_attributes_for :reviews, :allow_destroy => true
-	# validates_presence_of :images
-	# validates_presence_of :descriptions
-	# validates_presence_of :reviews
+  validates :Name, presence: true
+  validates :Description, presence: true
+  
+  accepts_nested_attributes_for :images, :allow_destroy => true
+  accepts_nested_attributes_for :descriptions, :allow_destroy => true
+  accepts_nested_attributes_for :reviews, :allow_destroy => true
 
-	scope :item_search, -> (query) { 
-	 		joins("LEFT JOIN descriptions ON descriptions.item_id = items.id")
-	 		.joins("LEFT JOIN images ON images.item_id = items.id")
-	 		.where("\"Name\" like ? OR \"Description\" like ? OR descriptions.\"Title\" like ? OR descriptions.\"Content\" like ? OR images.\"Title\" like ? OR images.\"Content\" like ?", 
-	 	"%#{query}%","%#{query}%","%#{query}%","%#{query}%","%#{query}%","%#{query}%")
-	 		.group('items.id') }
-
+  scope :item_search, -> (query) { 
+      joins("LEFT JOIN descriptions ON descriptions.item_id = items.id")
+      .joins("LEFT JOIN images ON images.item_id = items.id")
+      .where("\"Name\" like ? OR \"Description\" like ? OR descriptions.\"Title\" like ? OR descriptions.\"Content\" like ? OR images.\"Title\" like ? OR images.\"Content\" like ?", 
+    "%#{query}%","%#{query}%","%#{query}%","%#{query}%","%#{query}%","%#{query}%")
+      .group('items.id') }
+      
+  def create_slug
+    self.slug = self.Name.parameterize
+  end
 end
