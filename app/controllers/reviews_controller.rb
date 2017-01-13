c7987df67e01da877cba294aa9aeee0074cec303
Erica Bista <ericabista77@gmail.com>
2017-01-13 12:02:16 +1100

    Update reviews_controller.rb

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,31 +1,32 @@
 class ReviewsController < ApplicationController
   before_filter :authorize, only: [:update, :create]
-def index
+
+  def index
     @reviews = Review.all
   end
+
   def create
-
-@review = Review.new(review_params)
-    respond_to do |format|
-      @review.user_id = current_user.id if current_user
-      if @review.save 
-        format.html { redirect_to slugged_path(Item.find_by_id(@review.item_id).slug), notice: 'Review was successfully created.' }
-        format.json { render :show, status: :created, location: @review }
-      else
-        format.html { redirect_to slugged_path(Item.find_by_id(@review.item_id).slug) }
-        format.json { render json: @review.errors, status: :unprocessable_entity }
+    @review = Review.new(review_params)
+      respond_to do |format|
+        @review.user_id = current_user.id if current_user
+        if @review.save 
+          format.html { redirect_to slugged_path(Item.find_by_id(@review.item_id).slug), notice: 'Review was successfully created.' }
+          format.json { render :show, status: :created, location: @review }
+        else
+          format.html { redirect_to slugged_path(Item.find_by_id(@review.item_id).slug) }
+          format.json { render json: @review.errors, status: :unprocessable_entity }
+        end
       end
-    end
   end
-   def review_params
-      params.require(:review).permit(:title, :description, :item_id, :parent_id, :user_id)
-    end
 
-    def authorize
+  def review_params
+    params.require(:review).permit(:title, :description, :item_id, :parent_id, :user_id)
+  end
+
+  def authorize
     unless current_user
       flash[:notice] = "Login to Post Review !!"
       redirect_to new_user_session_path
     end
   end
 end
-
