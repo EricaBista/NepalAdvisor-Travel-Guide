20daec0d3594efb1e5f4676ed61d19ba5fb5bce7
Erica Bista <ericabista77@gmail.com>
2017-01-13 12:06:42 +1100

    Update likes_controller.rb

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,34 +1,35 @@
 class LikesController < InheritedResources::Base
-def index
+
+  def index
     @likes = Like.all
   end
   
   def create
-
-@like = Like.new(like_params)
-    respond_to do |format|
-      @like.user_id = current_user.id if current_user
-      if @like.save 
-        format.html { redirect_to slugged_path(Item.find_by_id(@like.item_id).slug), notice: 'Liked successfully.' }
-        format.json { render :show, status: :created, location: @like }
-      else
-        format.html { redirect_to item_path(@like.item_id) }
-        format.json { render json: @like.errors, status: :unprocessable_entity }
+    @like = Like.new(like_params)
+      respond_to do |format|
+        @like.user_id = current_user.id if current_user
+        if @like.save 
+          format.html { redirect_to slugged_path(Item.find_by_id(@like.item_id).slug), notice: 'Liked successfully.' }
+          format.json { render :show, status: :created, location: @like }
+        else
+          format.html { redirect_to item_path(@like.item_id) }
+          format.json { render json: @like.errors, status: :unprocessable_entity }
+        end
       end
-    end
   end
+
   def destroy
-  	@like = Like.find(params[:id])
+    @like = Like.find(params[:id])
     @like.destroy
     respond_to do |format|
       format.html { redirect_to slugged_path(Item.find_by_id(@like.item_id).slug), notice: 'like was successfully destroyed.' }
       format.json { head :no_content }
     end
-end
+  end
+
   private
 
-    def like_params
-      params.require(:like).permit(:user_id, :item_id)
-    end
+  def like_params
+    params.require(:like).permit(:user_id, :item_id)
+  end
 end
-
