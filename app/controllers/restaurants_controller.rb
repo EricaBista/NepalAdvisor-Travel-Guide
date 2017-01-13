841c7f62c9992fc321dbd614867fb66dde58bd8f
Erica Bista <ericabista77@gmail.com>
2017-01-13 12:03:52 +1100

    Update restaurants_controller.rb

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,9 +1,7 @@
 class RestaurantsController < InheritedResources::Base
-	
   private
-
-    def restaurant_params
-      params.require(:restaurant).permit(:Name, :Title, :Content)
-    end
+  
+  def restaurant_params
+    params.require(:restaurant).permit(:Name, :Title, :Content)
+  end
 end
-
