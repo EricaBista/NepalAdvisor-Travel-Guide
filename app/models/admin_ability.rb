d00ce07be3f6f23f7f3561ab93941cab2c26f045
Erica Bista <ericabista77@gmail.com>
2017-01-13 14:51:28 +1100

    Update admin_ability.rb

--- app/models/admin_ability.rb
+++ app/models/admin_ability.rb
@@ -3,13 +3,10 @@ class AdminAbility
 
   def initialize(user)
     user ||= User.new
-
-    # A moderator can manage the blog posts
     if user.role?('editor')
       can :manage, Item
     end
-
-    # An administrator can manage the blog posts and manage users
+    
     if user.role?('administrator')
       can :manage, User
     end
