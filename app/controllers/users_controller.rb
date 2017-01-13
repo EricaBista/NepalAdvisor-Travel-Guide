71cb967af1c2aceb33a34a7cf26f64ada0079d2f
Erica Bista <ericabista77@gmail.com>
2017-01-13 11:57:53 +1100

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,68 +1,59 @@
 class UsersController < ApplicationController
 	before_action :set_user, only: [:show, :edit, :update, :destroy]
-   before_filter :authenticate_user!
+  before_filter :authenticate_user!
   load_and_authorize_resource skip_load_resource only: [:create] 
-  def edit
-    @user = current_user
+  
+	def edit
+  	@user = current_user
   end
 
   def update_password
     @user = User.find(current_user.id)
-    # puts "test test test test test test"
     if @user.update(user_params)
-      # Sign in the user by passing validation in case their password changed
-      sign_in @user, :bypass => true
+    	sign_in @user, :bypass => true
       redirect_to root_path
     else
-
       render "update_password"
     end
   end
    
   def profile
     @user = User.find_by_id(current_user)
+  end
 
-    end
+  def update
+		respond_to do |format|
+			if @user.update(user_params)
+				format.html { redirect_to profile_path, notice: 'user was successfully updated.' }
+				format.json { render :show, status: :ok, location: @user }
+			else
+				format.html { render :edit }
+				format.json { render json: @user.errors, status: :unprocessable_entity }
+			end
+		end
+  end
 
-    def update
-       # @picture = @user.picture.build(user_params)
-      respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to profile_path, notice: 'user was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-     end
-    end
+  def new
+  	@user = User.new
+  end
 
-  	def new
-    	  @user = User.new
-    end
+  def create
+ 		@user = User.new(user_params)
+		if @user.save
+  		session[:user_id] = @user.id
+      redirect_to root_url, notice: "Thank you for signing in"
+  	else
+  		render "new"
+  	end
+  end
 
-    def create
-  	    @user = User.new(user_params)
-  	   
-  	    if @user.save
-           
-  	    	session[:user_id] = @user.id
-  		 
-         redirect_to root_url, notice: "Thank you for signing in"
-  	    else
-  		 render "new"
-  	    end
-    end
-
-
-
-
-    def set_user
-      @user = User.find(params[:id])
-    end
-    def user_params
-      params.require(:user).permit(:email, :password, :password_confirmation, :current_password, :picture, :full_name)
-    end
-
-
+ 	def set_user
+  	@user = User.find(params[:id])
+  end
+	
+	private
+	
+  def user_params
+  	params.require(:user).permit(:email, :password, :password_confirmation, :current_password, :picture, :full_name)
+  end
 end
