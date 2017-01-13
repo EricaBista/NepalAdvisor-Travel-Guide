aef2cfdb4e7c72e8ce64d44b711991b62b71cdec
Erica Bista <ericabista77@gmail.com>
2017-01-13 14:53:14 +1100

    Update category.rb

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,17 +1,15 @@
 class Category < ActiveRecord::Base
-	validates :Name, presence: true
-	validates :Description, presence: true
-	validates :slug, uniqueness: true
-	has_many :items, :dependent => :destroy
-	mount_uploader :icon, IconUploader
-	mount_uploader :banner, AddUploader
-	before_save :create_slug
-
-	  def create_slug
-	    self.slug = self.Name.parameterize
-	  end
-	  scope :category_search, -> (query) { 
-	 		
-	 		where("\"Name\" like ? OR \"Description\" like ? ","%#{query}%","%#{query}%") }
-
+  validates :Name, presence: true
+  validates :Description, presence: true
+  validates :slug, uniqueness: true
+  has_many :items, :dependent => :destroy
+  mount_uploader :icon, IconUploader
+  mount_uploader :banner, AddUploader
+  before_save :create_slug
+  
+  scope :category_search, -> (query) { where("\"Name\" like ? OR \"Description\" like ? ","%#{query}%","%#{query}%") }
+  
+  def create_slug
+      self.slug = self.Name.parameterize
+  end 
 end
